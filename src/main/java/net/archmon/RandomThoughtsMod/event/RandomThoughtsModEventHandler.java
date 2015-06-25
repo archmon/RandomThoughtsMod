91e6fe049bdc5267bdcfd32f3b4758bc7c83b4cf
archmon <archmon@users.noreply.github.com>
2015-06-25 17:22:14 -0500

    MineMaarten video 10 @sideonly done

--- src/main/java/net/archmon/RandomThoughtsMod/event/RandomThoughtsModEventHandler.java
+++ src/main/java/net/archmon/RandomThoughtsMod/event/RandomThoughtsModEventHandler.java
@@ -1,10 +1,14 @@
 package net.archmon.RandomThoughtsMod.event;
 
+import java.util.List;
+
 import net.archmon.RandomThoughtsMod.init.ModBlocks;
-import net.archmon.RandomThoughtsMod.utility.LogHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
@@ -12,6 +16,13 @@ import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import cpw.mods.fml.relauncher.Side;
 
 public class RandomThoughtsModEventHandler{
+
+	/**
+	 * Using the LivingDropsEvent. Note that this is from  net.minecraftforge.event, 
+	 * so to make this method be called, this class needs to be registered at 
+	 * MinecraftForge.EVENT_BUS.register(new RandomThoughtsModEventHandler()).
+	 * @param event
+	 */
 	@SubscribeEvent
 	public void addPigDrops(LivingDropsEvent event){
 		if(event.entityLiving instanceof EntityPig && event.entityLiving.getRNG().nextInt(3) == 0) {
@@ -20,10 +31,25 @@ public class RandomThoughtsModEventHandler{
 		}
 	}
 
+	/**
+	 * Using the PlayerTickEvent. Note that this is from cpw.mods.fml.common.gameevent, 
+	 * so to make this method be called, this class needs to be registered at 
+	 * FMLCommonHandler.instance().bus().register(new RandomThoughtsModEventHandler()).
+	 * @param event
+	 */
 	@SubscribeEvent
 	public void onPlayerTick(PlayerTickEvent event){
 		if(event.side == Side.SERVER && event.phase == TickEvent.Phase.END) {
-			LogHelper.info("Player " + event.player.getCommandSenderName() + " ticked" + event.phase);
+			//LogHelper.info("Player " + event.player.getCommandSenderName() + " ticked" + event.phase);
+			List<Entity> entities = event.player.worldObj.getEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(event.player.posX - 3, event.player.posY - 3, event.player.posZ - 3, event.player.posX + 3, event.player.posY + 3, event.player.posZ + 3));
+			for(Entity entity : entities) {
+				if(entity != event.player) {
+					//entity.setVelocity(0, 1, 0); //This will crash when run on a dedicated server, because Entity#setVelocity is marked with @SideOnly(Side.CLIENT), and therefore stripped from a dedicated server instance.
+					entity.motionX = 0;//The solution is to do this.
+					entity.motionY = 1;
+					entity.motionZ = 0;
+				}
+			}
 		}
 	}
 }
