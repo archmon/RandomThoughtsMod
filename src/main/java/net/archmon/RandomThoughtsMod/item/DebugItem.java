ffe2d2a80f2776087831c76ed53288c651ca7bab
archmon <archmon@users.noreply.github.com>
2015-07-04 11:12:01 -0500

    Made debug tool tell user how much light a block emits.

--- src/main/java/net/archmon/RandomThoughtsMod/item/DebugItem.java
+++ src/main/java/net/archmon/RandomThoughtsMod/item/DebugItem.java
@@ -1,5 +1,12 @@
 package net.archmon.RandomThoughtsMod.item;
 
+import net.archmon.RandomThoughtsMod.utility.LogHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.World;
+
 /**
  * code by archmon
  */
@@ -12,5 +19,18 @@ public class DebugItem extends Item_RTM{
 		//don't forget to go to en_US.lang and ModItems.java
 		maxStackSize = 1;
 		//makes it to where each item is a full stack.
+	}
+
+	@Override
+	public boolean onItemUse(ItemStack IStack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10){
+		int lightLevel;
+		lightLevel = world.getBlockLightValue(x, y, z);
+
+		LogHelper.info(lightLevel);
+		if(!world.isRemote) {
+			player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.GREEN + "Light level at " + x + ", " + y + ", " + z + " is " + lightLevel));
+		}
+		return true; // we don't want to use the item.
+
 	}
 }
